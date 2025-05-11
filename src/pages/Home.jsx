54469038e106698bc4e8eef9582243311f0a78d1
ClauM5 <92365231+ClauM5@users.noreply.github.com>
2025-05-11 01:23:12 -0300

    Primeiro commit do frontend

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Hortifruti Delivery</h1>
+      <p>Seu app está funcionando!</p>
+    </div>
+  );
+}
+
+export default Home;
